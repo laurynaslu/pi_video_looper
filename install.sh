8a6bd3aa83456eaf3a0062b748eace4388943443
Laurynas Lubys <laurynas.lubys@gmail.com>
2017-04-09 21:13:05 +0300

    Shallow clone pi_hello_video

--- install.sh
+++ install.sh
@@ -16,7 +16,7 @@ apt-get -y install build-essential python-dev python-pip python-pygame superviso
 
 echo "Installing hello_video..."
 echo "========================="
-git clone https://github.com/adafruit/pi_hello_video.git
+git clone --depth=1 https://github.com/adafruit/pi_hello_video.git
 cd pi_hello_video
 ./rebuild.sh
 cd hello_video
